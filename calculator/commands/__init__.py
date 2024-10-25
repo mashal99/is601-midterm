990b9fb32eba498b50acca785654d0bdbde6769d
Ahmed Mashaal <46572957+mashal99@users.noreply.github.com>
2024-10-24 21:37:10 -0400

    add Menu Command

--- calculator/commands/__init__.py
+++ calculator/commands/__init__.py
@@ -42,3 +42,15 @@ class CommandHandler:
         
         command = self.commands[name]
         return command.execute(*args)
+    
+class MenuCommand(Command):
+    """
+    Command to display all available commands dynamically.
+    """
+    def __init__(self, command_handler):
+        self.command_handler = command_handler
+
+    def execute(self, *args):
+        print("Available Commands:")
+        for command_name in self.command_handler.commands.keys():
+            print(f"- {command_name}")
